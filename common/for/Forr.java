e9646c9b054f2e44b7c764c0f45a5ce3a060e065
Karl Li <gangli@vw-mobvoi.com>
2019-11-05 11:20:33 +0800

    init 0

---
+++ common/for/Forr.java
@@ -0,0 +1,7 @@
+public class Forr {
+	public static void main(String[] args) {
+		for (int i = 0; i < 9; i++) {
+			System.out.println("123456");
+		}
+	}
+}
---
+++ common/hello/Hello.java
@@ -0,0 +1,9 @@
+
+import java.io.PrintStream;
+
+public class Hello {
+	public static void main(String[] args) {
+		PrintStream out = System.out;
+		out.println("Hello");
+	}
+}
